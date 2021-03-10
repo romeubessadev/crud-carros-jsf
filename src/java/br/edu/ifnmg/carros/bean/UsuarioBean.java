394c3e3a4dc25b38fdf68cbf31f6f85a9e1cfb61
= <=>
2021-03-10 15:36:10 -0300

    Primeiro commit

---
+++ src/java/br/edu/ifnmg/carros/bean/UsuarioBean.java
@@ -0,0 +1,27 @@
+package br.edu.ifnmg.carros.bean;
+
+import br.edu.ifnmg.carros.dao.UsuarioDAO;
+import br.edu.ifnmg.carros.entidade.Usuario;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class UsuarioBean extends CrudBean<Usuario, UsuarioDAO>{
+
+    private UsuarioDAO usuarioDAO;
+    
+    @Override
+    public UsuarioDAO getDao() {
+        if(usuarioDAO == null){
+            usuarioDAO = new UsuarioDAO();
+        }
+        return usuarioDAO;
+    }
+
+    @Override
+    public Usuario criarNovaEntidade() {
+        return new Usuario();
+    }
+    
+}
